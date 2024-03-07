c00f221f376296077fed7d248710fc5800d4f3f6
Ranjan Kumar <vtu16964@veltech.edu.in>
2024-03-07 12:49:17 +0530

    Header Completed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
+import react from "react";
 import './App.css';
-
+import Header from "./Header";
 function App() {
   return (
+    // BEM naming convention
     <div className="App">
       <h1>Hello Interrupt coder !!!</h1>
       {/* header */}
+      <Header/>
       {/* sidebar */}
       {/* React-Router -> Chat screen */}
-      {/* header */}
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -1,18 +1,24 @@
-import React from 'react'
-import './Header.css'
+//import React from 'react';
+import './Header.css';
+import {Avatar} from "@mui/material";
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
+//import PersonIcon from '@mui/icons-material/Person';
+import SearchIcon from '@mui/icons-material/Search';
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
 function Header() {
   return (
     <div className='header'>
       <div className="header_left">
-        {/* Avatar for logged in user */}
-        {/* Time Icon */}
+        <Avatar className='header_avatar' alt="Ranjan Kumar" src=""/>
+        <AccessTimeIcon />
       </div>
       <div className="header_search">
       {/* Search Icon */}
-      {/* input */}
+      <SearchIcon/>
+      <input type="text" placeholder='Search Interrupt Coder' />
       </div>
       <div className="header_right">
-      {/** Help icon */}
+      <HelpOutlineIcon/>
       </div>
     </div>
   )
